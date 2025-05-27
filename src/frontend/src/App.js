7d79788f35a6527197d67195295f88c20c174b21
Marie Alette Stenhaug <mst301@uit.no>
2025-05-27 11:08:21 +0200

    trying to fix gitignores :/

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,17 @@
 import logo from './assets/GenAi-Logo-2.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/header';
+import Hero from './components/hero';
+import CookieBanner from './components/cookieBanner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+      <Hero />
+      <CookieBanner />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -12,8 +19,9 @@ function App() {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noOpener noReferrer">
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
+    
   );
 }
 
