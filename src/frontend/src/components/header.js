fb32e8739d81a24be747a8f34d599de444a5a7c5
Marie Alette Stenhaug <mst301@uit.no>
2025-05-27 13:01:10 +0200

    small fixes to css

--- src/frontend/src/components/header.js
+++ src/frontend/src/components/header.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import '../css/header.css';
+import logo from '../assets/logo.svg';
 
 export default function Header() {
   return (
     <header className="header">
       <div className="logo">
+        <img src={logo} alt="Logo" className="logo-icon" />
         Your <strong>AI</strong> Tutor
       </div>
-      <nav>
-        <ul>
+      <nav className="nav-center">
+        <ul className="nav-links">
           <li>Flashcard generation</li>
           <li>Talk to it</li>
+          <li>Procrastinate</li>
           <li>About the team behind AiT</li>
         </ul>
       </nav>
-      <div className="actions">
-        <a href="#contact">Q&A</a>
-        <button className="button">Frequently asked questions</button>
+      <div className="header-right">
+        <button className="cta-button">Frequently asked questions</button>
       </div>
     </header>
   );
